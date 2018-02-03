2abd5d2775c33ab9d2e97a248dcd7dc3e7ace6e8
Unknown <b.leech@hotmail.ca>
2018-02-03 00:14:33 -0600

    cleanup

--- Assets/TobiiDrawing/ClickBrush.cs
+++ Assets/TobiiDrawing/ClickBrush.cs
@@ -11,12 +11,13 @@ public class ClickBrush : MonoBehaviour {
 	Vector2 startPos;
 	Plane objPlane;
 
-	Vector3 prevPos;
+	//Vector3 prevPos;
 
-	public float alpha = 0.3f;
+	public float multiplier = 0.3f;
 	private Vector2 _historicPoint;
 	private bool _hasHistoricPoint;
 
+    // Called at the start of the scene
 	void Start(){
 		objPlane = new Plane(Camera.main.transform.forward*-1, this.transform.position);
 	}
@@ -25,12 +26,10 @@ public class ClickBrush : MonoBehaviour {
 	void Update () {
 
 		GazePoint gazePoint = TobiiAPI.GetGazePoint();
-		HeadPose headPose = TobiiAPI.GetHeadPose ();
 
 		if (Input.GetKeyDown (KeyCode.Space)) {
 
 			thisBrush = (GameObject)Instantiate (brushPrefab, this.transform.position, Quaternion.identity);
-            //thisBrush.GetComponent<TrailRenderer>().widthMultiplier = (headPose.Position.z / 100);
 
 			Ray mRay = Camera.main.ScreenPointToRay (gazePoint.Screen);
 
@@ -38,38 +37,34 @@ public class ClickBrush : MonoBehaviour {
             _hasHistoricPoint = false;
 			if (objPlane.Raycast (mRay, out rayDistance)) {
 				startPos = mRay.GetPoint (rayDistance);
-				prevPos = mRay.GetPoint (rayDistance);
+				//prevPos = mRay.GetPoint (rayDistance);
 			}
 		} else if (Input.GetKey (KeyCode.Space) && gazePoint.IsRecent ()) {
 			Ray mRay = Camera.main.ScreenPointToRay (gazePoint.Screen);
 
 			float rayDistance;
 			if (objPlane.Raycast (mRay, out rayDistance)) {
-				//thisBrush.transform.position = ((mRay.GetPoint (rayDistance)) + prevPos) / 2;
-				thisBrush.transform.position = smoothify(new Vector2(mRay.GetPoint(rayDistance).x,mRay.GetPoint(rayDistance).y));
-				//thisBrush.transform.position = (mRay.GetPoint(rayDistance));
+				thisBrush.transform.position = smoothFilter(new Vector2(mRay.GetPoint(rayDistance).x,mRay.GetPoint(rayDistance).y));
 			}
 
-			prevPos = mRay.GetPoint (rayDistance);
+			//prevPos = mRay.GetPoint (rayDistance);
 		} else if (Input.GetKey (KeyCode.Space)) {
 			if (Vector2.Distance (thisBrush.transform.position, startPos) < 0.1)
             {
                 Destroy (thisBrush);
                 _hasHistoricPoint = false;
-            }
-				
+            }	
 		}
-			
 	}
 
-	private Vector2 smoothify(Vector2 point){
+	private Vector2 smoothFilter(Vector2 point){
 		if (!_hasHistoricPoint) {
 			_historicPoint = point;
 			_hasHistoricPoint = true;
 		}
 
-		var smoothedPoint = new Vector2 (point.x * alpha + _historicPoint.x * (1.0f - alpha), 
-			                    point.y * alpha + _historicPoint.y * (1.0f - alpha));
+		var smoothedPoint = new Vector2 (point.x * multiplier + _historicPoint.x * (1.0f - multiplier), 
+			                    point.y * multiplier + _historicPoint.y * (1.0f - multiplier));
 
 		_historicPoint = smoothedPoint;
 
