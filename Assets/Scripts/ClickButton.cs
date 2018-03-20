dd5fb58509b3e71d13ad4c7a77b41a52d8ec5b3a
jdesnoyers <johnny.desnoyers@gmail.com>
2018-03-20 13:29:38 -0600

    Add files via upload

---
+++ Assets/Scripts/ClickButton.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Tobii.Gaming;
+
+public class ClickButton : MonoBehaviour {
+
+    public GameObject brushObject;
+    public GameObject brushManager;
+
+	// Use this for initialization
+	void Start () {
+		if(brushObject==null)
+        {
+            brushObject = transform.GetChild(0).gameObject;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        GameObject focusedObject = TobiiAPI.GetFocusedObject();
+
+        if(focusedObject == gameObject)
+        {
+            brushManager.GetComponent<PlaceObject>().drawObject = brushObject;
+        }
+        
+
+    }
+}
